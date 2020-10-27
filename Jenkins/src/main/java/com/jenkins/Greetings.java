ff43fda62be8783d592978e11f7161ed05ab4879
Sandiiprathod <48466050+Sandiiprathod@users.noreply.github.com>
2020-10-27 17:52:58 +0530

    Add files via upload

---
+++ Jenkins/src/main/java/com/jenkins/Greetings.java
@@ -0,0 +1,10 @@
+package com.jenkins;
+
+public class Greetings {
+
+	
+	public String greet(String str) {
+		return "Hello " + str +" !!!";
+		
+	}
+}
---
+++ Jenkins/src/main/java/com/jenkins/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.jenkins;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	Greetings obj= new Greetings();
+	
+	System.out.println(obj.greet("Jenkins"));
+}
+}
